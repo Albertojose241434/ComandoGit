92db9118ec9bce4737a9359c888787a55c58e452
Albertojose241434 <albertojose241434@gmail.com>
2023-03-16 15:48:42 -0500

    restablecido

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 let nombre, correo, mensaje;
-let formulario = documento.getElementById('form')
+let formulario = document.getElementById('form')
+
 
 formulario.addEventListener('submit', (e)=>{
     e.preventDefault()
@@ -7,10 +8,13 @@ formulario.addEventListener('submit', (e)=>{
 })
 
 function LeerData() {
-    nombre = document.getElementById("nombre").value;
-    correo = document.getElementById("correo").value;
-    mensaje = document.querySelector("#textarea").value;
-    console.log(nombre)
+    nombre = document.getElementById('nombre').value;
+    correo = document.getElementById('correo').value;
+    mensaje = document.getElementById('textarea').value;
+
+    ValidarData(nombre,correo,mensaje)
+    GuardarLocalStorage(nombre,correo,mensaje)
+   
 }
 function ValidarData(nombre, correo, mensaje) {
     if (nombre.length==0 || correo.length==0 || mensaje.length==0) {
@@ -18,9 +22,24 @@ function ValidarData(nombre, correo, mensaje) {
         Swal.fire ({
             title: 'Error!',
             Text: 'do you want to continue',
-            icon: 'Error',
+            icon: 'error',
             confirmButtonText: 'Cool',
             iconColor:'purple'
         })
     }
 }
+
+  function GuardarLocalStorage(nombre,correo,mensaje) {
+    localStorage.setItem('Nombre',nombre)
+    localStorage.setItem('Correo',correo)
+    localStorage.setItem('Mensaje',mensaje)
+    ListarData()
+    }
+
+    function ListarData() {
+        let nombreUsu = localStorage.getItem('Nombre')
+        let correoUsu = localStorage.getItem('Correo')
+        let mensajeUsu = localStorage.getItem('Mensaje')
+        
+    }
+   
